d442c2b1bc58aff368906da8d52f881d09925a5f
blockchainunchained <none@example.com>
2019-09-17 18:39:09 +0100

    Add error handling to image_generator.

--- image_generator/generate.js
+++ image_generator/generate.js
@@ -16,41 +16,46 @@ async function run () {
       ]
     });
     console.log("Opening page")
-    const page = await browser.newPage();
-    await page.setViewport({width: width, height: 208, deviceScaleFactor: 2});
-    let tickerResponse = await page.goto(url, {
-      waitUntil: "networkidle2"
-    });
-    if(tickerResponse.status() === 200){
-      console.log("Saving ticker screenshots.");
-      await page.screenshot({
-        clip: {x:0, y:0, width:width, height:height/2},
-        path: "/data/upper-ticker.png",
-        omitBackground: true
+    try {
+      const page = await browser.newPage();
+      await page.setViewport({width: width, height: 208, deviceScaleFactor: 2});
+      let tickerResponse = await page.goto(url, {
+        waitUntil: "networkidle2"
       });
-      await page.screenshot({
-        clip: {x:0, y:height/2, width:width, height:height/2},
-        path: "/data/lower-ticker.png",
-        omitBackground: true
+      if(tickerResponse.status() === 200){
+        console.log("Saving ticker screenshots.");
+        await page.screenshot({
+          clip: {x:0, y:0, width:width, height:height/2},
+          path: "/data/upper-ticker.png",
+          omitBackground: true
+        });
+        await page.screenshot({
+          clip: {x:0, y:height/2, width:width, height:height/2},
+          path: "/data/lower-ticker.png",
+          omitBackground: true
+        });
+      } else {
+        console.log("Error fetching data for ticker screenshots.");
+      }
+      let bannerResponse = await page.goto(url + "?style=true", {
+        waitUntil: "networkidle2"
       });
-    } else {
-      console.log("Error fetching data for ticker screenshots.");
+      if(bannerResponse.status() === 200){
+        console.log("Saving banner screenshot.");
+        await page.screenshot({
+          clip: {x:0, y:0, width:width, height:208},
+          path: "/data/banner.png"
+        });
+      } else {
+        console.log("Error fetching data for banner screenshot.");
+      }
+      await page.close({});
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      await browser.close();
+      console.log("Waiting 300 seconds.")
     }
-    let bannerResponse = await page.goto(url + "?style=true", {
-      waitUntil: "networkidle2"
-    });
-    if(bannerResponse.status() === 200){
-      console.log("Saving banner screenshot.");
-      await page.screenshot({
-        clip: {x:0, y:0, width:width, height:208},
-        path: "/data/banner.png"
-      });
-    } else {
-      console.log("Error fetching data for banner screenshot.");
-    }
-    await page.close({});
-    await browser.close();
-    console.log("Waiting 300 seconds.")
 }
 console.log("Waiting 10 seconds.")
 setTimeout(run, 10 * 1000);
